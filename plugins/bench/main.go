60800f3dfae649df6045723fce2d621810757116
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2017-03-28 18:10:48 -0400

    Make initial commit and set up hello-world slackbot
    The hello-world slack bot now pings the local server 3 times and sends the
    response and prints the commands.

---
+++ plugins/bench/main.go
@@ -0,0 +1,35 @@
+package bench
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/go-chat-bot/bot"
+)
+
+func gibberish(command *bot.Cmd) (msg string, err error) {
+
+	fmt.Println("cmd: " + command.Command)
+	fmt.Println("msg: " + command.Message)
+	fmt.Println("arg: ", command.Args)
+	msg = "asdf awrf asdf asf sf"
+
+	cmd := exec.Command("ping", "-c 4", "127.0.0.1")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	msg = "# pinging \n" + "``` \n" + out.String() + "\n ```"
+
+	return
+}
+
+func init() {
+
+	fmt.Println("init")
+	bot.RegisterCommand("bench", "testing bot.", "blabber", gibberish)
+}
